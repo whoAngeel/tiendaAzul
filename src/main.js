be18bdb13198c90520e4e4c63ab1732e3594bd27
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2023-11-19 15:12:07 -0600

    Instalando fontawesome icon

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import { router } from './router'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
