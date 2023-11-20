54704a28b8e8741c331da8df6455754401f4e003
HpLiz <lizbethhzpz@gmail.com>
2023-11-19 23:15:41 -0600

    tablas principales

--- src/Views/Proveedores.vue
+++ src/Views/Proveedores.vue
@@ -1,8 +1,27 @@
 <template>
-    <Navbar title="Proveedores" />
-    <div class="h-full flex justify-center items-center content-center">Proveedores</div>
+  <Navbar title="Proveedores" />
+
+  <div class="flex justify-between w-3/4 mx-auto mt-4">
+    <button class="btn flex items-center">
+      <font-awesome-icon :icon="['fas', 'plus']" />Agregar
+    </button>
+    <div class="flex">
+      <span class="inline-flex items-center px-3 text-sm">
+        <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+      </span>
+      <input
+        id="website-admin"
+        class="input block flex-1 min-w-0 w-full text-sm"
+        placeholder="Buscar"
+      />
+    </div>
+  </div>
+  <div class="flex justify-between w-3/4 mx-auto mt-4">
+    <ProveedorCard> </ProveedorCard>
+  </div>
 </template>
 
 <script setup>
-import Navbar from '../components/navbar/Navbar.vue';
+import Navbar from "../components/navbar/Navbar.vue";
+import ProveedorCard from "../components/ProveedorCard.vue";
 </script>
