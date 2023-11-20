d7cda0bd4c85200e915c5b0aa57f1a04e7c27417
HpLiz <lizbethhzpz@gmail.com>
2023-11-19 20:44:12 -0600

    tabla en vista productos

--- src/Views/Usuarios.vue
+++ src/Views/Usuarios.vue
@@ -37,89 +37,27 @@
               <div class="avatar">
                 <div class="mask mask-squircle w-12 h-12">
                   <img
-                    src="/tailwind-css-component-profile-2@56w.png"
+                  src="http://placekitten.com/g/300/300"
                     alt="Avatar Tailwind CSS Component"
                   />
                 </div>
               </div>
               <div>
-                <div class="font-bold">Hart Hagerty</div>
-                <div class="text-sm opacity-50">United States</div>
+                <div class="font-bold">Mancha</div>
               </div>
             </div>
           </td>
           <td>
-            Zemlak, Daniel and Leannon
+            9511423658
             <br />
-            <span class="badge badge-ghost badge-sm"
-              >Desktop Support Technician</span
-            >
           </td>
-          <td>Purple</td>
-          <td>Purple</td>
+          <td>mancha@manchillas.com</td>
+          <td>Administradora</td>
           <th>
             <button class="btn btn-ghost btn-xs">details</button>
           </th>
         </tr>
-        <!-- row 2 -->
-        <tr>
-          <td>
-            <div class="flex items-center gap-3">
-              <div class="avatar">
-                <div class="mask mask-squircle w-12 h-12">
-                  <img
-                    src="/tailwind-css-component-profile-3@56w.png"
-                    alt="Avatar Tailwind CSS Component"
-                  />
-                </div>
-              </div>
-              <div>
-                <div class="font-bold">Brice Swyre</div>
-                <div class="text-sm opacity-50">China</div>
-              </div>
-            </div>
-          </td>
-          <td>
-            Carroll Group
-            <br />
-            <span class="badge badge-ghost badge-sm">Tax Accountant</span>
-          </td>
-          <td>Red</td>
-          <td>Purple</td>
-          <th>
-            <button class="btn btn-ghost btn-xs">details</button>
-          </th>
-        </tr>
-        <!-- row 3 -->
-        <tr>
-          <td>
-            <div class="flex items-center gap-3">
-              <div class="avatar">
-                <div class="mask mask-squircle w-12 h-12">
-                  <img
-                    src="/tailwind-css-component-profile-4@56w.png"
-                    alt="Avatar Tailwind CSS Component"
-                  />
-                </div>
-              </div>
-              <div>
-                <div class="font-bold">Marjy Ferencz</div>
-                <div class="text-sm opacity-50">Russia</div>
-              </div>
-            </div>
-          </td>
-          <td>
-            Rowe-Schoen
-            <br />
-            <span class="badge badge-ghost badge-sm">Office Assistant I</span>
-          </td>
-          <td>Crimson</td>
-          <td>Purple</td>
-          <th>
-            <button class="btn btn-ghost btn-xs">details</button>
-          </th>
-        </tr>
-        <!-- row 4 -->
+        
       </tbody>
       <!-- foot -->
       <tfoot>
